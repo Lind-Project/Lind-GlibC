5961ee8f0c9d9ff0127234f3e50384fa654d7572
aKpkc <11726205+kapkic@users.noreply.github.com>
2021-09-10 20:29:26 -0400

    Change call number

--- sysdeps/nacl/nacl_syscalls.h
+++ sysdeps/nacl/nacl_syscalls.h
@@ -142,7 +142,7 @@
 #define NACL_sys_getsockopt             137
 #define NACL_sys_setsockopt             138
 
-#define NACL_sys_getsockname            140
+#define NACL_sys_getsockname            144
 
 
 #define NACL_MAX_SYSCALLS               256
