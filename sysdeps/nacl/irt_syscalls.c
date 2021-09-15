c6aac9130c03d3f6fa1bf1fe98e40edda1e6a078
jesings <34111484+jesings@users.noreply.github.com>
2021-09-14 14:50:06 -1000

    Fixed issues in select implementation

--- sysdeps/nacl/irt_syscalls.c
+++ sysdeps/nacl/irt_syscalls.c
@@ -398,8 +398,7 @@ int (*__nacl_irt_ppoll) (struct pollfd *fds, nfds_t nfds,
     const struct timespec *timeout, const sigset_t *sigmask,
         size_t sigset_size, int *count);
 int (*__nacl_irt_select) (int nfds, fd_set *readfds,
-    fd_set *writefds, fd_set *exceptfds, const struct timeval *timeout,
-        int *count);
+    fd_set *writefds, fd_set *exceptfds, const struct timeval *timeout);
 int (*__nacl_irt_pselect) (int nfds, fd_set *readfds,
     fd_set *writefds, fd_set *exceptfds, const struct timeval *timeout,
         void* sigmask, int *count);
@@ -514,14 +513,9 @@ static int nacl_irt_getegid(void) {
 }
 
 static int nacl_irt_select_lind (int nfds, fd_set *readfds,
-    fd_set *writefds, fd_set *exceptfds, const struct timeval *timeout,
-    int *count)
+    fd_set *writefds, fd_set *exceptfds, const struct timeval *timeout)
 {
-    int rv = NACL_SYSCALL (select) (nfds, readfds, writefds, exceptfds, timeout);
-    if (rv < 0)
-        return -rv;
-    *count = rv;
-    return 0;
+    return NACL_SYSCALL (select) (nfds, readfds, writefds, exceptfds, timeout);
 }
 
 static int nacl_irt_socket_lind (int domain, int type, int protocol, int *sd)
