008cf4360159a97536911f91bdf82ecf6380a4f1
rennergade <nr2185@nyu.edu>
2021-09-17 15:43:39 -0400

    poll

--- sysdeps/nacl/poll.c
+++ sysdeps/nacl/poll.c
@@ -5,7 +5,7 @@
 int
 __poll(struct pollfd *fds, nfds_t nfds, int timeout)
 {
-  int result = nacl_irt_poll (fds, nfds, timeout);
+  int result = __nacl_irt_poll (fds, nfds, timeout);
   if (result < 0) {
      __set_errno (-result);
      return -1;
