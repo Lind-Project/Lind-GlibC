cf14758b89fc19be2336e644dd7a778629f20710
Nicholas Renner <nr2185@nyu.edu>
2021-12-29 17:56:05 -0500

    Lstat Hook (#41)
    * lstat hook
    
    * lstat hook
    
    * lstat hook

--- make_sysd_rules.py
+++ make_sysd_rules.py
@@ -170,7 +170,6 @@ override_list = [
     "sysdeps/nacl/wait.c",
     "sysdeps/nacl/wait4.c",
     "sysdeps/nacl/sigprocmask.c",
-    "sysdeps/nacl/lstat.c",
     "sysdeps/nacl/gethostname.c",
     "sysdeps/nacl/pread.c",
     "sysdeps/nacl/pwrite.c"
