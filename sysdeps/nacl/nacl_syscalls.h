ef0fdd4196c28a6c15314e794fc289c0ae8f913d
Nicholas Renner <nicholassrenner@gmail.com>
2022-02-22 20:48:11 -0500

    syscall

--- sysdeps/nacl/nacl_syscalls.h
+++ sysdeps/nacl/nacl_syscalls.h
@@ -179,6 +179,7 @@ struct timespec;
 
 #define socklen_t unsigned int
 
+typedef int (*TYPE_nacl_syscall)(...);
 typedef int (*TYPE_nacl_nameservice)(int *desc_in_out);
 typedef int (*TYPE_nacl_link)(char *from, char *to);
 typedef int (*TYPE_nacl_unlink)(char *name);
