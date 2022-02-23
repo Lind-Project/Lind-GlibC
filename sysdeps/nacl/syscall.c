097dafefa0deef9382af4d71a5973bbb23655ba6
Nicholas Renner <nicholassrenner@gmail.com>
2022-02-22 22:20:21 -0500

    syscall

--- sysdeps/nacl/syscall.c
+++ sysdeps/nacl/syscall.c
@@ -11,7 +11,7 @@ int __syscall (int callnum, ...) {
     va_list argptr;
     va_start(argptr, callnum);
     int dummy = 0;
-    int result = -(NACL_SYSCALL_ADDR (callnum)) (argptr);
+    int result = -(*(NACL_SYSCALL_ADDR (callnum)))(argptr);
     va_end(argptr);
 
     if (result < 0) {
