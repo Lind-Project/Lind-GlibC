1b8b48a5f5ccd327ae6f5c44e3ac227606c90a74
Nicholas Renner <nr2185@nyu.edu>
2022-06-30 17:23:19 -0400

    Add syscall() (#51)
    * syscall
    
    * syscall
    
    * print'
    
    * metadatfix
    
    * syscall
    
    * syscall
    
    * syscall
    
    * syscall
    
    * syscall
    
    * syscall
    
    * syscall
    
    * syscall
    
    * syscall
    
    * syscall
    
    * syscall
    
    * syscall
    
    * syscall
    
    * error check
    
    * error check
    
    * error check
    
    * error check
    
    * error check
    
    * merge

---
+++ sysdeps/nacl/syscall.c
@@ -0,0 +1,34 @@
+#include <errno.h>
+#include <stdarg.h>
+#include <nacl_syscalls.h>
+#include <irt_syscalls.h>
+
+long int __syscall (long int __sysno, ...) {
+
+
+    if (__sysno < 0 || __sysno > NACL_MAX_SYSCALLS) {
+      __set_errno (ENOSYS);
+      return -1;
+    }
+
+    va_list args;
+    va_start(args, __sysno);
+    long arg1 = va_arg(args, long);
+    long arg2 = va_arg(args, long); 
+    long arg3 = va_arg(args, long); 
+    long arg4 = va_arg(args, long); 
+    long arg5 = va_arg(args, long); 
+    long arg6 = va_arg(args, long); 
+
+    int result = ((TYPE_nacl_syscall) NACL_SYSCALL_ADDR (__sysno)) (arg1, arg2, arg3, arg4, arg5, arg6);
+    va_end(args);
+
+    if (result < 0) {
+        __set_errno (-result);
+        return -1;
+    }
+
+    return result;
+}
+
+weak_alias (__syscall, syscall)
