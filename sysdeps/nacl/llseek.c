738e6f274e4c2b90f012a148fb84b05274ba093c
Nicholas Renner <nr2185@nyu.edu>
2023-09-30 21:54:23 -0400

    Lseek revert (#77)
    * revert lseek
    
    * revert lseek

--- sysdeps/nacl/llseek.c
+++ sysdeps/nacl/llseek.c
@@ -1,4 +1,3 @@
-
 #include <errno.h>
 
 #include <irt_syscalls.h>
@@ -6,13 +5,12 @@
 
 loff_t __llseek (int fd, loff_t offset, int whence)
 {
-  int result = __nacl_irt_seek (fd, offset, whence);
-  if (result < 0)
+  int result = __nacl_irt_seek (fd, offset, whence, &offset);
+  if (result != 0)
     {
-      __set_errno(-result);
+      errno = result;
       return -1;
     }
   return offset;
 }
 weak_alias (__llseek, llseek)
-
